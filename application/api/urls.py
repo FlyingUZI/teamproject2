a57f53a7ffedc4c1cca06646a679fff5940b616a
ErgoFriend <ergoproxy2236@yahoo.co.jp>
2018-11-09 14:58:18 +0900

    api test

--- application/api/urls.py
+++ application/api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
-    path('info/', views.info, name='info')
+    path('test/', views.test, name='test')
 ]
--- application/api/views.py
+++ application/api/views.py
@@ -7,12 +7,5 @@ from django.core.exceptions import SuspiciousOperation
 VERIFICATION_TOKEN = '***********************'
 
 
-def info(request):
-    if request.method != 'POST':
-        return JsonResponse({})
-
-    if request.POST.get('token') != VERIFICATION_TOKEN:
-        raise SuspiciousOperation('Invalid request.')
-
-    user_name = request.POST['user_name']
-    user_id = request.POST['user_id']
+def test(request):
+    return "test"
